797340ec600c39d566d955029ff839eb96397099
andrew040 <31290684+andrew040@users.noreply.github.com>
2017-10-16 23:00:23 +0000

    Added GPX output

--- gps.py
+++ gps.py
@@ -15,6 +15,16 @@ key = open('key','w')
 key.write(str(last_key))
 key.close()
 
+file = open('/var/www/html/testfile'+str(last_key)+'.txt','w')
+
+file.write('<gpx xmlns="http://www.topografix.com/GPX/1/1" xmlns:gpxx="http://www.garmin.com/xmlschemas/GpxExtensions/v3" xmlns:gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1" creator="Oregon 400t" version="1.1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd">\n')
+file.write('  <trk>\n')
+file.write('    <name>GPX Track</name>\n')
+file.write('    <trkseg>\n')
+
+file.close()
+
+print('File: '+str(last_key))
 while True:
     file = open('/var/www/html/testfile'+str(last_key)+'.txt','a') 
     line = ser.readline() 
@@ -32,20 +42,42 @@ while True:
     if datavalid > 4:
      if gprmctime == gpggatime:
       if gpggatime != previous_time:
-       #print gprmcdata[9] + ' - ' + gprmcdata[1][:6]
-       file.write(gprmcdata[1][:6] + ',') # UTC time without trailing zero's
-       file.write(gprmcdata[2] + ',') # Fix valid
-       file.write(gprmcdata[3] + ',') # latitude
-       file.write(gprmcdata[4] + ',') # north/south
-       file.write(gprmcdata[5] + ',') # longitude
-       file.write(gprmcdata[6] + ',') # east/west
-       file.write(gprmcdata[7] + ',') # speed
-       file.write(gprmcdata[8] + ',') # true track
-       file.write(gprmcdata[9] + ',') # date
-       file.write(gpggadata[7] + ',') # number of satellites
-       file.write(gpggadata[8] + ',') # hdop
-       file.write(gpggadata[9]) # altitude
-       file.write('\r\n')
+       atomtime = '20'+gprmcdata[9][4:6]+'-'+gprmcdata[9][2:4]+'-'+gprmcdata[9][0:2]+'T'
+       atomtime = atomtime + gprmcdata[1][:2]+':'+ gprmcdata[1][2:4]+':'+ gprmcdata[1][4:6]+'Z'
+       latdeg = gprmcdata[3][:2]
+       latdec = gprmcdata[3][2:]
+       lat = float(latdeg)+float(latdec)/60
+
+       londeg = gprmcdata[5][:3]
+       londec = gprmcdata[5][3:]
+       lon = float(londeg)+float(londec)/60
+
+#       lat = float(gprmcdata[3])/100
+#       lon = float(gprmcdata[5])/100
+       file.write('      <trkpt lat="'+str(lat)+'" lon="'+str(lon)+'">\n')
+       file.write('        <ele>'+gpggadata[9]+'</ele>\n')
+       file.write('        <time>'+atomtime+'</time>\n')
+       file.write('        <hdop>'+gpggadata[8]+'</hdop>\n')
+       file.write('        <sat>'+gpggadata[7]+'</sat>\n')
+       file.write('        <geoidheight>'+gpggadata[11]+'</geoidheight>\n')
+
+       file.write('      </trkpt>\n')
+
+
+
+#       file.write(gprmcdata[1][:6] + ',') # UTC time without trailing zero's
+#       file.write(gprmcdata[2] + ',') # Fix valid
+#       file.write(gprmcdata[3] + ',') # latitude
+#       file.write(gprmcdata[4] + ',') # north/south
+#       file.write(gprmcdata[5] + ',') # longitude
+#       file.write(gprmcdata[6] + ',') # east/west
+#       file.write(gprmcdata[7] + ',') # speed
+#       file.write(gprmcdata[8] + ',') # true track
+#       file.write(gprmcdata[9] + ',') # date
+#       file.write(gpggadata[7] + ',') # number of satellites
+#       file.write(gpggadata[8] + ',') # hdop
+#       file.write(gpggadata[9]) # altitude
+#       file.write('\r\n')
        previous_time = gpggatime
     file.close()
 ser.close()             # close port
